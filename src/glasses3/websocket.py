9ba23126a966d6f75cd1c0f58b2ed6f0cbaa5f9f
Markus Wesslén <mw2013@tobii.com>
2022-06-09 17:09:16 +0200

    Add receiver prototype

--- src/glasses3/websocket.py
+++ src/glasses3/websocket.py
@@ -1,3 +1,5 @@
+import asyncio
+import json
 import websockets
 import websockets.client
 
@@ -11,9 +13,22 @@ class G3WebSocketClientProtocol(websockets.client.WebSocketClientProtocol):
 
     def __init__(self, subprotocols, **kwargs):
         self._message_count = 0
-        self._signals_map = {}
         self._message_map = {}
+        self._signals_map = {}
+        self._event_loop = asyncio.get_running_loop()
+        self._recv = asyncio.create_task(self._receiver())
         if not subprotocols:
             subprotocols = self.DEFAULT_SUBPROTOCOLS
         super().__init__(subprotocols=subprotocols, **kwargs)
-     
+    
+    async def _receiver(self):
+        async for message in self:
+            json_message = json.load(message)
+            self._message_map[json_message['id']].set_result(json_message)
+    
+    async def require(self, request):
+        self._message_count += 1
+        request['id'] = self._message_count
+        await self.send(json.dump(request))
+        future = self._message_map[self._message_count] = self._event_loop.create_future()
+        return await future
