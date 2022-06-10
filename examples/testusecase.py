2c3be52803fef869a82af2707631956df96671c1
Markus Wesslén <mw2013@tobii.com>
2022-06-10 15:10:09 +0200

    Fix receiver prototype and update example

--- examples/testusecase.py
+++ examples/testusecase.py
@@ -1,31 +1,40 @@
 import websockets
-from glasses3.websocket import G3WebSocketClientProtocol, connect
+import glasses3.websocket
 import json
 import asyncio
 
+g3_hostname = "tg02b-080105022801"
+test_request = '{"path":"/recorder", "method":"GET"}'
 
-class G3:
-    recorder = '{"path":"/recorder","id":42,"method":"GET"}'
-    
-class Recorder:
-    def start():
-        pass
+
+async def use_case_1():
+    async with glasses3.websocket.connect(g3_hostname) as g3ws:
+        g3ws.start_receiver_task()
+        response = await g3ws.require(test_request)
+        response2 = await g3ws.require(test_request)
+        print("uc1-r1", json.dumps(response, indent=2))
+        print("uc1-r2", json.dumps(response2, indent=2))
+
+
+async def use_case_2():
+    async with websockets.connect(
+        "ws://{}/websockets".format(g3_hostname),
+        create_protocol=glasses3.websocket.G3WebSocketClientProtocol,
+    ) as g3ws:
+        g3ws.start_receiver_task()
+        response = await g3ws.require(test_request)
+        response2 = await g3ws.require(test_request)
+        print("uc2-r1", json.dumps(response, indent=2))
+        print("uc2-r2", json.dumps(response2, indent=2))
+
 
 async def handler():
-    # print('print:', getattr(await G3WebSocketClientProtocol.connect('tg02b-080105022801'), '__aenter__'))
-    async with websockets.connect('ws://tg02b-080105022801/websockets', create_protocol=G3WebSocketClientProtocol) as ws:
-        await ws.send(G3.recorder)
-        answer = await ws.recv()
-        print(json.dumps(json.loads(answer), indent=2))
-    async with connect('tg02b-080105022801') as g3ws:
-        await g3ws.send(G3.recorder)
-        answer = await g3ws.recv()
-        print(json.dumps(json.loads(answer), indent=2))
+    await asyncio.gather(use_case_1(), use_case_2())
 
 
 def main():
     asyncio.run(handler())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/glasses3/websocket.py
+++ src/glasses3/websocket.py
@@ -6,29 +6,40 @@ import websockets.client
 
 def connect(g3_hostname, wspath="/websocket"):
     ws_uri = "ws://{}{}".format(g3_hostname, wspath)
-    return websockets.connect(ws_uri, create_protocol=G3WebSocketClientProtocol, subprotocols=G3WebSocketClientProtocol.DEFAULT_SUBPROTOCOLS)
+    return websockets.connect(
+        ws_uri,
+        create_protocol=G3WebSocketClientProtocol,
+        subprotocols=G3WebSocketClientProtocol.DEFAULT_SUBPROTOCOLS,
+    )
+
 
 class G3WebSocketClientProtocol(websockets.client.WebSocketClientProtocol):
-    DEFAULT_SUBPROTOCOLS = ['g3api']
+    DEFAULT_SUBPROTOCOLS = ["g3api"]
 
-    def __init__(self, subprotocols, **kwargs):
+    def __init__(self, *, subprotocols=None, **kwargs):
         self._message_count = 0
         self._message_map = {}
         self._signals_map = {}
         self._event_loop = asyncio.get_running_loop()
-        self._recv = asyncio.create_task(self._receiver())
         if not subprotocols:
             subprotocols = self.DEFAULT_SUBPROTOCOLS
         super().__init__(subprotocols=subprotocols, **kwargs)
-    
-    async def _receiver(self):
+
+    def start_receiver_task(self):
+        self._receiver = asyncio.create_task(self._receiver_task())
+
+    async def _receiver_task(self):
         async for message in self:
-            json_message = json.load(message)
-            self._message_map[json_message['id']].set_result(json_message)
-    
+            json_message = json.loads(message)
+            self._message_map[json_message["id"]].set_result(json_message)
+
     async def require(self, request):
         self._message_count += 1
-        request['id'] = self._message_count
-        await self.send(json.dump(request))
-        future = self._message_map[self._message_count] = self._event_loop.create_future()
+        json_request = json.loads(request)
+        json_request["id"] = self._message_count
+        updated_request = json.dumps(json_request)
+        await self.send(updated_request)
+        future = self._message_map[
+            self._message_count
+        ] = self._event_loop.create_future()
         return await future
