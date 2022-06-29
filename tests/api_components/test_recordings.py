c816c350fa1f3b812328c4d551a86b95099b9482
Oskar Söderberg <os2012@tobii.com>
2022-06-29 14:40:13 +0200

    Minor code clean up

--- tests/api_components/test_recordings.py
+++ tests/api_components/test_recordings.py
@@ -1,4 +1,3 @@
-import asyncio
 from typing import List, cast
 
 import pytest
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import AsyncIterable
 
-import pytest_asyncio
+import pytest
 
 from glasses3 import Glasses3
 from glasses3.g3typing import Hostname
@@ -9,7 +9,7 @@ from glasses3.g3typing import Hostname
 g3_hostname = Hostname("tg02b-080105022801")
 
 
-@pytest_asyncio.fixture(scope="module")
+@pytest.fixture(scope="module")
 async def g3() -> AsyncIterable[Glasses3]:
     async with Glasses3.connect(g3_hostname) as g3:
         await g3.recordings.start_children_handler_tasks()
@@ -17,7 +17,7 @@ async def g3() -> AsyncIterable[Glasses3]:
         await g3.recordings.stop_children_handler_tasks()
 
 
-@pytest_asyncio.fixture(scope="module")
+@pytest.fixture(scope="module")
 def event_loop():
     loop = asyncio.new_event_loop()
     yield loop
