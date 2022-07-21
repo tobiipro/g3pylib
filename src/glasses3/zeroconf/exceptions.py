688d69113ac54d2e1b3ddbea2949d5f62dab0d2d
Oskar Söderberg <os2012@tobii.com>
2022-07-21 15:58:34 +0200

    Add in-order event handling and events property
    Ordered handling prevents service events racing each other

--- src/glasses3/zeroconf/exceptions.py
+++
@@ -1,6 +0,0 @@
-class ServiceDiscoveryError(Exception):
-    """Raised when service discovery is unsuccessful."""
-
-
-class ServiceEventError(Exception):
-    """Raised when a service is removed or updated before addition."""
