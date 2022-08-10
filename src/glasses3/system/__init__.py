eb45372bef939ca79917278464269f0e66c839c6
Oskar Söderberg <os2012@tobii.com>
2022-08-10 15:25:24 +0200

    Implement system endpoints and tests

--- src/glasses3/system/__init__.py
+++ src/glasses3/system/__init__.py
@@ -1,4 +1,5 @@
-from typing import Optional, cast
+from datetime import datetime
+from typing import List, Optional, cast
 
 from glasses3.g3typing import URI
 from glasses3.system.battery import Battery
@@ -18,10 +19,105 @@ class System(APIComponent):
             self._battery = Battery(self._connection, URI(self._api_uri + "/battery"))
         return self._battery
 
+    async def get_head_unit_serial(self) -> str:
+        return cast(
+            str,
+            await self._connection.require_get(
+                self.generate_endpoint_uri(EndpointKind.PROPERTY, "head-unit-serial")
+            ),
+        )
+
     async def get_name(self) -> str:
         return cast(
             str,
             await self._connection.require_get(
                 self.generate_endpoint_uri(EndpointKind.PROPERTY, "name")
+            ),
+        )
+
+    async def get_ntp_is_enabled(self) -> bool:
+        return cast(
+            bool,
+            await self._connection.require_get(
+                self.generate_endpoint_uri(EndpointKind.PROPERTY, "ntp-is-enabled")
+            ),
+        )
+
+    async def get_ntp_is_synchronized(self) -> bool:
+        return cast(
+            bool,
+            await self._connection.require_get(
+                self.generate_endpoint_uri(EndpointKind.PROPERTY, "ntp-is-synchronized")
+            ),
+        )
+
+    async def get_recording_unit_serial(self) -> str:
+        return cast(
+            str,
+            await self._connection.require_get(
+                self.generate_endpoint_uri(
+                    EndpointKind.PROPERTY, "recording-unit-serial"
+                )
+            ),
+        )
+
+    async def get_time(self) -> datetime:
+        date_time = cast(
+            str,
+            await self._connection.require_get(
+                self.generate_endpoint_uri(EndpointKind.PROPERTY, "time")
+            ),
+        )
+        return datetime.fromisoformat(date_time.strip("Z"))
+
+    async def get_timezone(self) -> str:
+        return cast(
+            str,
+            await self._connection.require_get(
+                self.generate_endpoint_uri(EndpointKind.PROPERTY, "timezone")
+            ),
+        )
+
+    async def get_version(self) -> str:
+        return cast(
+            str,
+            await self._connection.require_get(
+                self.generate_endpoint_uri(EndpointKind.PROPERTY, "version")
+            ),
+        )
+
+    async def available_gaze_frequencies(self) -> List[int]:
+        return cast(
+            List[int],
+            await self._connection.require_post(
+                self.generate_endpoint_uri(
+                    EndpointKind.ACTION, "available-gaze-frequencies"
+                )
+            ),
+        )
+
+    async def set_time(self, value: datetime) -> bool:
+        return cast(
+            bool,
+            await self._connection.require_post(
+                self.generate_endpoint_uri(EndpointKind.ACTION, "set-time"),
+                body=[f"{datetime.isoformat(value)}Z"],
+            ),
+        )
+
+    async def set_timezone(self, value: str) -> bool:
+        return cast(
+            bool,
+            await self._connection.require_post(
+                self.generate_endpoint_uri(EndpointKind.ACTION, "set-timezone"),
+                body=[value],
+            ),
+        )
+
+    async def use_ntp(self, value: bool) -> bool:
+        return cast(
+            bool,
+            await self._connection.require_post(
+                self.generate_endpoint_uri(EndpointKind.ACTION, "use-ntp"), body=[value]
             ),
         )
---
+++ tests/api_components/test_system.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+from glasses3 import Glasses3
+
+
+async def test_get_head_unit_serial(g3: Glasses3):
+    assert type(await g3.system.get_head_unit_serial()) is str
+
+
+async def test_get_name(g3: Glasses3):
+    assert type(await g3.system.get_name()) is str
+
+
+async def test_get_ntp_is_enabled(g3: Glasses3):
+    assert type(await g3.system.get_ntp_is_enabled()) is bool
+
+
+async def test_get_ntp_is_synchronized(g3: Glasses3):
+    assert type(await g3.system.get_ntp_is_synchronized()) is bool
+
+
+async def test_get_recording_unit_serial(g3: Glasses3):
+    assert type(await g3.system.get_recording_unit_serial()) is str
+
+
+async def test_get_time(g3: Glasses3):
+    assert type(await g3.system.get_time()) is datetime
+
+
+async def test_get_timezone(g3: Glasses3):
+    assert type(await g3.system.get_timezone()) is str
+
+
+async def test_get_version(g3: Glasses3):
+    assert type(await g3.system.get_version()) is str
+
+
+async def test_available_gaze_frequencies(g3: Glasses3):
+    available_gaze_frequencies = await g3.system.available_gaze_frequencies()
+    assert type(available_gaze_frequencies) is list
+    assert type(available_gaze_frequencies[0]) is int
+
+
+async def test_use_ntp_and_set_time(g3: Glasses3):
+    assert await g3.system.use_ntp(False)
+    assert await g3.system.set_time(
+        datetime.fromisoformat("2000-01-01T00:00:00.000000")
+    )
+    assert await g3.system.use_ntp(True)
+
+
+async def test_set_timezone(g3: Glasses3):
+    assert await g3.system.set_timezone("Europe/Stockholm")
+    assert await g3.system.set_timezone("CET")
