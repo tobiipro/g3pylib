a04b2d453ed2ad077fdd06a111c464bde0abf80e
Oskar Söderberg <os2012@tobii.com>
2022-08-16 16:39:26 +0200

    Change test order, see #53

--- tests/test_connect_to_glasses.py
+++ tests/test_connect_to_glasses.py
@@ -12,6 +12,14 @@ def g3_hostname() -> str:
     return os.environ["G3_HOSTNAME"]
 
 
+async def test_connect_with_hostname_using_zeroconf_and_ip(g3_hostname: str):
+    async with connect_to_glasses.with_hostname(
+        g3_hostname, using_zeroconf=True, using_ip=True
+    ) as g3:
+        serial = await g3.system.get_recording_unit_serial()
+        assert type(serial) is str
+
+
 async def test_connect_with_service_using_ip(g3_hostname: str):
     g3_service = await G3ServiceDiscovery.request_service(g3_hostname)
     async with connect_to_glasses.with_service(g3_service, using_ip=True) as g3:
@@ -22,14 +30,6 @@ async def test_connect_with_service_using_ip(g3_hostname: str):
 async def test_connect_with_service_using_hostname(g3_hostname: str):
     g3_service = await G3ServiceDiscovery.request_service(g3_hostname)
     async with connect_to_glasses.with_service(g3_service, using_ip=False) as g3:
-        serial = await g3.system.get_recording_unit_serial()
-        assert type(serial) is str
-
-
-async def test_connect_with_hostname_using_zeroconf_and_ip(g3_hostname: str):
-    async with connect_to_glasses.with_hostname(
-        g3_hostname, using_zeroconf=True, using_ip=True
-    ) as g3:
         serial = await g3.system.get_recording_unit_serial()
         assert type(serial) is str
 
