c67e5f11bdeef9e514615eac23d6d09c62e643ea
Markus Wesslén <markus.wesslen@gmail.com>
2022-08-17 11:49:22 +0200

    Add example showing service info

---
+++ examples/zeroconfinfo.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+import os
+
+import dotenv
+
+from glasses3 import connect_to_glasses
+from glasses3.zeroconf import G3ServiceDiscovery
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def zeroconf_info():
+    service = await G3ServiceDiscovery.request_service(os.environ["G3_HOSTNAME"])
+    logging.info(f"Received service {service}")
+
+
+def main():
+    asyncio.run(zeroconf_info())
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    main()
